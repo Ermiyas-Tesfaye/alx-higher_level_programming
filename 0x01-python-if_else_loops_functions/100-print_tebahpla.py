42ad8aa8e8e116b01b697f80f96819e3c5683403
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-09-20 14:13:48 +0000

    pycode style is corrected

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 num = 122
-for i in range (26):
+for i in range(26):
     if num % 2 == 0:
         print(chr(num), end="")
     else:
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@ def uppercase(str):
     i = 0
     value = 0
 
-    for i in range (len(str)):
+    for i in range(len(str)):
         if ord(str[i]) > 96 and ord(str[i]) < 123:
             value = ord(str[i]) - 32
         else:
