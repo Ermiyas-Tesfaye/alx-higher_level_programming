d8fd4dab494f4523bc4298e2c3818d870ecde21b
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-09-25 09:20:18 +0000

    Up

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     i = 0
     for elm in range(len(my_list)):
-        print(format(my_list[::-1][i]))
+        print("{}".format(my_list[::-1][i]))
         i += 1
