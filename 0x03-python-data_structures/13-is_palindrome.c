bce164fc64a298bb254c605b781397b734fca2f6
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-09-25 17:06:11 +0000

    New file is added

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/*+
+ * is_palindrome - a fun.. that checks if singly linked list is a palindrom
+ * @head: head pointer to singly linked list
+ * Return: 1 0r 0
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
