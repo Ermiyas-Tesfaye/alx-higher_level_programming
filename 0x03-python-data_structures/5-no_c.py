d7f7d0b720efa52a233adfa53b067bbb3ee7ad7b
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-09-25 10:18:11 +0000

    Update

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
+    for char in my_string:
+        if (not (char == 'c')) and (not (char == 'C')):
+            new_str += char
+    return new_str
