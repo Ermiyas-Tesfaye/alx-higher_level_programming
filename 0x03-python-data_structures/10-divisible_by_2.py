64e51d9ff7349cc5c01d2b2343147a0c274251fc
Ermiyas-Tesfaye <ermidbu@gmail.com>
2022-09-25 13:10:13 +0000

    New update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    idx = 0
+    for i in my_list:
+        if i % 2 == 0:
+            new_list[idx] = True
+        else:
+            new_list[idx] = False
+        idx += 1
+    return new_list
